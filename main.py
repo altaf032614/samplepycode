97fc5f91fa0d621b331b906c6c33d0e261079bc9
Abdulla.Ghouse <Abdulla.Ghouse@qsc.com>
2025-03-21 18:34:04 -0700

    Teaming with Altaf

---
+++ main.py
@@ -0,0 +1,15 @@
+# using numpy generate a sine wave with 1 khz frequency and 1 second duration
+
+import numpy as np
+
+# Write a simple function to generate a sine wave with a given frequency and duration
+
+def generate_sine_wave(frequency, duration, sample_rate):
+    t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
+    wave = np.sin(2 * np.pi * frequency * t)
+    return wave
+
+wave = generate_sine_wave(1000, 2, 100)
+print(type(wave))
+print(wave.shape)
+print (wave)
